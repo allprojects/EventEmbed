951ea2d13dca3123e41f4d8201342ccb2f12496b
viering <viering@users.noreply.github.com>
2013-11-25 01:36:46 +0100

    added a more complex joint test

--- src/test/scala/TypeLevelSpec.scala
+++ src/test/scala/TypeLevelSpec.scala
@@ -19,6 +19,19 @@ class TypeLevelSpec extends FunSpec with Matchers{
     type cmp2 = Compare[_2,_1]
     type cmp3 = Compare[_2,_2]
     type cmps = cmp2 :: cmp1 :: HNil
+    
+    
+    
+    type cs = Compare[_3,_7] :: Compare[_2,_2] :: Compare[_0,_0] :: HNil
+    type c2=Compare[_2,_2] ::  Compare[_3,_7]:: Compare[_0,_0] :: HNil
+    type t1 = ::[Int, ::[Boolean, ::[String, ::[Option[Int], ::[String, HNil]]]]]
+    type t2 =  ::[Int, ::[Int, ::[String, ::[Option[String], ::[Float, ::[Double, ::[Object, ::[Option[Int], HNil]]]]]]]]
+    type t2Removed = ::[Int, ::[Option[String], ::[Float, ::[Double, ::[Object,  HNil]]]]]
+    type t3 = ::[Int, ::[Boolean, ::[String, ::[Option[Int], ::[String, t2Removed]]]]]
+    
+    implicitly[Join.Aux[t1,t2, cs,t3]]
+    //implicitly[Join.Aux[t1,t2, c2,t3]]
+    
     type l4 = ::[Int, ::[Int, ::[String, ::[Int, ::[String, HNil]]]]]
     type l6 = ::[Int, ::[Int, ::[String, HNil]]]
     
@@ -40,6 +53,8 @@ class TypeLevelSpec extends FunSpec with Matchers{
     type h1 = ::[Int, HNil]
     type h2 = ::[Int, ::[Int , HNil]]
     type h3 = ::[Int, ::[Int , HNil]]
+    
+    
     type c =  ::[cmp1, HNil]
     		
     //implicitly[RemoveIndex.Aux[l6, _1, l2]]
@@ -50,7 +65,7 @@ class TypeLevelSpec extends FunSpec with Matchers{
     implicitly[Join[h1,h1,HNil]]
     
     
-    val t : Join.Aux[h1,h2, cmp1 :: HNil,h2] = Join.recCase[_0,_0,h1,h2,HNil, Int, h1]
+    //val t : Join.Aux[h1,h2, cmp1 :: HNil,h2] = Join.recCase[_0,_0,h1,h2,HNil, Int, h1]
      
     implicitly[Join.Aux[h1,h1,HNil ,h3]]
     implicitly[Join.Aux[h1,h2, cmp1 :: HNil,h2]]
